27529af7d87cbdf509512bafb2b831e51f5a2947
Ricardo Kizaki <ricardokizaki@hotmail.com>
2023-10-05 21:44:08 -0300

    Animal Model

---
+++ WebApplication1/WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: Animal
+        public ActionResult Index()
+        {
+            return View(AnimalModel.CriarAnimal());
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(AnimalModel.CriarAnimal()[id]);
+        }
+
+        // GET: Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(AnimalModel.CriarAnimal()[id]);
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(AnimalModel.CriarAnimal()[id]);
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/AnimalModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AnimalModel
+    {
+        public string Nome { get; set; }
+        public string Dono { get; set; }
+        public int Idade { get; set; }
+
+        public static List<AnimalModel> CriarAnimal()
+        {
+            var lista = new List<AnimalModel>();
+
+            lista.Add(new AnimalModel() { Nome = "Fifi", Dono = "Fernanda", Idade = 1 });
+            lista.Add(new AnimalModel() { Nome = "Max", Dono = "Rafael", Idade = 5 });
+            lista.Add(new AnimalModel() { Nome = "Leão", Dono = "Gabriel", Idade = 3 });
+
+            return lista;
+        }
+    }
+}
